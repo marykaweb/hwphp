1babc3419bc898d88def0789605558ece2185702
maryka <marykaweb@gmail.com>
2018-06-15 23:01:15 +0300

    array

---
+++ index.php
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<a href="forarray.php">PHP - 2. Основы PHP. Массивы</a>
+</body>
+</html>
