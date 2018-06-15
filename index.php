d881d72e7fd1b0cc19ee8d41eea605812cbd3233
maryka <marykaweb@gmail.com>
2018-06-15 23:37:02 +0300

    add asocial array

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
 	<title>Document</title>
 </head>
 <body>
-	<a href="forarray.php">PHP - 2. Основы PHP. Массивы</a>
+	<ul>
+	<li><a href="forarray.php">PHP - 2. Основы PHP. Массивы</a></li>
+	<li><a href="asocialarray.php">PHP - 3. Основы PHP. Ассоциативные массивы</a></li>
+	</ul>
 </body>
 </html>
