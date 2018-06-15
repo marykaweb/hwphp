f49dd0509e0c51346619a2cbbf890059a27d0692
maryka <marykaweb@gmail.com>
2018-06-16 00:17:26 +0300

    add function

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<ul>
 	<li><a href="forarray.php">PHP - 2. Основы PHP. Массивы</a></li>
 	<li><a href="asocialarray.php">PHP - 3. Основы PHP. Ассоциативные массивы</a></li>
+	<li><a href="basefunction.php">PHP - 4. Основы PHP. Функции	</a></li>
 	</ul>
 </body>
 </html>
