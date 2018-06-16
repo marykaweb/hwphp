b0f1dd6a55aebf8de0f3013d3a5b10dc0671bd00
maryka <marykaweb@gmail.com>
2018-06-16 12:55:43 +0300

    contact form

---
+++ contactform.php
@@ -0,0 +1,24 @@
+﻿<?php
+if ( !empty($_POST) ) {
+
+	$fullmessage = "Вам пришло новое сообщение с сайта:\n "
+	. "Имя отправителя: " . $_POST['username'] . "\n"
+	. "Email отправителя: " . $_POST['useremail'] . "\n"
+	. "Сообщение: \n " . $_POST['message'];
+
+	$resaultMail = mail("marykaweb@gmail.com", "Сообщение с сайта", $fullmessage);
+
+	if ( $resaultMail ) {
+		echo "Сообщение отправлено успешно";
+	} else {
+		echo "Упс, что-то пошло не так";
+	}
+}
+
+?>
+<form action="contactform.php" method="post">
+	<input type="text" name="username" placeholder="Ваше имя"><br>
+	<input type="text" name="useremail" placeholder="Ваша почта"><br>
+	<textarea name="message" id="" cols="30" rows="10" placeholder="Сообщение"></textarea><br>
+	<input type="submit" value="Отправить">
+</form>
--- index.php
+++ index.php
@@ -8,7 +8,8 @@
 	<ul>
 	<li><a href="forarray.php">PHP - 2. Основы PHP. Массивы</a></li>
 	<li><a href="asocialarray.php">PHP - 3. Основы PHP. Ассоциативные массивы</a></li>
-	<li><a href="basefunction.php">PHP - 4. Основы PHP. Функции	</a></li>
-	</ul>
+	<li><a href="basefunction.php">PHP - 4. Основы PHP. Функции</a></li>
+	<li><a href="contactform.php">PHP - 5. Post и Get запросы</a></li>
+</ul>
 </body>
 </html>
